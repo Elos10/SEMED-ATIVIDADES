6e279880da1d143b6a0720b6a03f1fd7fd5ce734
Eder Leonardo <140769660+Elos10@users.noreply.github.com>
2026-06-01 17:20:24 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = 'https://fcijhxqdfpykdsnbkatw.supabase.co';
-export const SUPABASE_ANON_KEY = 'sb_publishable_7w3w4Z3ytdAtNHdfqYUz7A_XQVludqR';
+export const SUPABASE_URL = 'https://gmcwbyeabochwcdjkaso.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_Vh09IP8Yo7DjfCxVYFeBPw_BPP0wcqk';
